ebcf200307985964309df197771c537729c73676
adammcgrogan <adam.mcgrogan@dailypay.com>
2026-05-10 15:32:09 +0100

    add CORS middleware for skpm.org

--- internal/server/server.go
+++ internal/server/server.go
@@ -18,6 +18,19 @@ func New(port string) *http.Server {
 
 	return &http.Server{
 		Addr:    fmt.Sprintf(":%s", port),
-		Handler: mux,
+		Handler: corsMiddleware(mux),
 	}
+}
+
+func corsMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", "https://skpm.org")
+		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
+		w.Header().Set("Access-Control-Allow-Headers", "Authorization, Content-Type")
+		if r.Method == http.MethodOptions {
+			w.WriteHeader(http.StatusNoContent)
+			return
+		}
+		next.ServeHTTP(w, r)
+	})
 }
